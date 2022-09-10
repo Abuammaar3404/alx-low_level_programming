edd296a1d37fab48d120c238c0b694c9ba572067
abuammaar3404 <you@muhammedtoheer@gmail.com>
2022-09-10 02:24:24 -0700

    printf

--- 0x00-hello_world/5-print.c
+++ 0x00-hello_world/5-print.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
 * main - print the string in the put function
 *
@@ -12,4 +11,5 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+
 
